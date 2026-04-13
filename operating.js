e261ad4a36abef6583863424504a583f4b6665bf
steveken15 <ken407104@gmail.com>
2026-04-14 00:33:34 +0100

    oper

---
+++ operating.js
@@ -0,0 +1,17 @@
+ let age = 22;
+    let  isAdult = true;
+if(age>=18 && isAdult) {
+    console.log("Elligible for the party");
+} else {
+    console.log("Not Elligible for the party")
+} 
+console.log("Party start at 9pm");
+
+const isAdmin = false;
+const isGuestuser = true;
+if(isAdmin || isGuestuser) {
+    console.log("Access granted");
+} else {
+    console.log("Access denied");
+}
+console.log("Welcome to steveken contribution group");
