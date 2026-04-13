38a59fc7747bba5058d55d340a058ae8255886f1
steveken15 <ken407104@gmail.com>
2026-04-14 00:13:31 +0100

    operators tools

---
+++ operators.js
@@ -0,0 +1,36 @@
+  //Arithematic operators
+
+//console.log("Addition" , 2+5);
+//console.log("Subtraction" , 9-6);
+//console.log("Multiplication" , 5*7);
+//console.log(Division , 10/2);
+//console.log(Modulus , 10%3);
+
+    //Comparison operators
+    //console.log("less than" , 5<9);
+    //console.log("greater than" ,8>11);
+    //console.log("less than or equal to" ,7<=7);
+    //console.log("greater than or equal to" , 10>=22);
+    //console.log("equal to" , 5==5);
+    //console.log("not equal to" , 6!=6);
+
+
+    let age = 22;
+    let  isAdult = true;
+if(age>=18 && isAdult) {
+    console.log("Elligible for the party");
+} else {
+    console.log("Not Elligible for the party")
+} 
+console.log("Party start at 9pm");
+
+const isAdmin = false;
+const isGuestuser = true;
+if(isAdmin || isGuestuser) {
+    console.log("Access granted");
+} else {
+    console.log("Access denied");
+}
+console.log("Welcome to steveken contribution group");
+
+
